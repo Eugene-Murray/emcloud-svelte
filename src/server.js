e6098b0ff08fcf29c1a2c4e985628af6ed3f25fa
Eugene Murray <eugene.murray@outlook.com>
2019-09-02 20:51:23 +0100

    update server

--- src/server.js
+++ src/server.js
@@ -4,7 +4,12 @@ import compression from 'compression';
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
+const API = 'https://hnpwa.com/api/v0';
 const dev = NODE_ENV === 'development';
+
+function load(type) {
+	return fetch(`${API}/${type}.json`).then(r => r.json());
+}
 
 polka() // You can also use Express
 	.use(
@@ -12,6 +17,13 @@ polka() // You can also use Express
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
+	.get('/:type?', async (req, res) => {
+		let type = req.params.type || 'news';
+		let data = await load(type).catch(err => {
+			send(res, 404);
+		});
+		send(res, 200, data);
+	})
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
