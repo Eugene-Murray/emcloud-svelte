c8d5b25814661d77341345b729584bead6f2e191
Eugene Murray <eugene.murray@gmail.com>
2019-11-03 19:46:06 +0000

    update for signals

--- src/routes/signals.svelte
+++ src/routes/signals.svelte
@@ -1,21 +1,444 @@
 <svelte:head>
-	<title>Signals</title>
+  <title>Signals</title>
 </svelte:head>
 
 <h1>Signals List</h1>
 <div class="container">
-	<div class="p-4">
+  <div class="p-4" style="border-style: solid">
 
-	<table border="1">
-	<tr>
-		<td>Symbol</td><td>Time Frame</td><td>Date/Time</td>
-	</tr>
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-		</tr>
-	</table>			  
+    <table border="1">
+      <tr>
+        <td colspan="3">Todays Signals</td>
+      </tr>
+      <tr>
+        <td>Symbol</td>
+        <td>Time Frame</td>
+        <td>Time</td>
+      </tr>
+      <tr>
+        <td />
+        <td />
+        <td />
+      </tr>
+    </table>
 
-			</div>
+  </div>
+
+  <div class="p-4" style="border-style: solid">
+
+    <table border="1">
+      <tr>
+        <td colspan="4">15 Min Signals</td>
+      </tr>
+      <tr>
+	  	<td></td>
+        <td>Symbol</td>
+        <td>Date/Time</td>
+        <td>Details</td>
+      </tr>
+      <tr>
+        <td>1.</td><td>OIL</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>2.</td><td>GOLD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>3.</td><td>SILVER</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>4.</td><td>DAX 30</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>5.</td><td>FSTE 100</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>6.</td><td>SPX 500</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>7.</td><td>NASDAQ 100</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>8.</td><td>DOW 30</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>9.</td><td>AUDNZD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>10.</td><td>AUDUSD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>11.</td><td>CADCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>12.</td><td>CADJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>13.</td><td>CHFJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>14.</td><td>EURCAD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>15.</td><td>EURCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>16.</td><td>EURGBP</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>17.</td><td>EURJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>18.</td><td>EURUSD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>19.</td><td>GBPCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>20.</td><td>GBPCAD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>21.</td><td>GBPJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>22.</td><td>GBPUSD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>23.</td><td>USDCAD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>24.</td><td>USDCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>25.</td><td>USDJPY</td><td> </td><td> </td>
+      </tr>
+    </table>
+
+  </div>
+
+    <div class="p-4" style="border-style: solid">
+
+    <table border="1">
+      <tr>
+        <td colspan="4">4 Hour Signals</td>
+      </tr>
+      <tr>
+	  	<td></td>
+        <td>Symbol</td>
+        <td>Date/Time</td>
+        <td>Details</td>
+      </tr>
+      <tr>
+        <td>1.</td><td>OIL</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>2.</td><td>GOLD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>3.</td><td>SILVER</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>4.</td><td>DAX 30</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>5.</td><td>FSTE 100</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>6.</td><td>SPX 500</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>7.</td><td>NASDAQ 100</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>8.</td><td>DOW 30</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>9.</td><td>AUDNZD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>10.</td><td>AUDUSD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>11.</td><td>CADCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>12.</td><td>CADJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>13.</td><td>CHFJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>14.</td><td>EURCAD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>15.</td><td>EURCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>16.</td><td>EURGBP</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>17.</td><td>EURJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>18.</td><td>EURUSD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>19.</td><td>GBPCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>20.</td><td>GBPCAD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>21.</td><td>GBPJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>22.</td><td>GBPUSD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>23.</td><td>USDCAD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>24.</td><td>USDCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>25.</td><td>USDJPY</td><td> </td><td> </td>
+      </tr>
+    </table>
+
+  </div>
+
+    <div class="p-4" style="border-style: solid">
+
+    <table border="1">
+      <tr>
+        <td colspan="4">1 Day Signals</td>
+      </tr>
+      <tr>
+	  	<td></td>
+        <td>Symbol</td>
+        <td>Date/Time</td>
+        <td>Details</td>
+      </tr>
+      <tr>
+        <td>1.</td><td>OIL</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>2.</td><td>GOLD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>3.</td><td>SILVER</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>4.</td><td>DAX 30</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>5.</td><td>FSTE 100</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>6.</td><td>SPX 500</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>7.</td><td>NASDAQ 100</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>8.</td><td>DOW 30</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>9.</td><td>AUDNZD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>10.</td><td>AUDUSD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>11.</td><td>CADCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>12.</td><td>CADJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>13.</td><td>CHFJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>14.</td><td>EURCAD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>15.</td><td>EURCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>16.</td><td>EURGBP</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>17.</td><td>EURJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>18.</td><td>EURUSD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>19.</td><td>GBPCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>20.</td><td>GBPCAD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>21.</td><td>GBPJPY</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>22.</td><td>GBPUSD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>23.</td><td>USDCAD</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>24.</td><td>USDCHF</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>25.</td><td>USDJPY</td><td> </td><td> </td>
+      </tr>
+    </table>
+
+  </div>
+
+  <div class="p-4" style="border-style: solid">
+
+    <table border="1">
+      <tr>
+        <td colspan="4">1 Day Signals - US Equity</td>
+      </tr>
+      <tr>
+	  	<td></td>
+        <td>Symbol</td>
+        <td>Date/Time</td>
+        <td>Details</td>
+      </tr>
+      <tr>
+        <td>1.</td><td>ABT - Abbott Laboratories</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>2.</td><td>ACM - Aecom</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>3.</td><td>AIG - American International Group Inc</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>4.</td><td>AMZN - Amazon.com, Inc.</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>5.</td><td>AXP - American Express Company</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>6.</td><td>AAPL - Apple Inc.</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>7.</td><td>BAC - Bank of America Corp</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>8.</td><td>BLK - BlackRock, Inc.</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>9.</td><td>BA - Boeing Co</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>10.</td><td>CAT - Caterpillar Inc</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>11.</td><td>CVX - Chevron Corporation</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>12.</td><td>C - Citigroup Inc</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>13.</td><td>KO - Coca-Cola Co</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>14.</td><td>DIS - Walt Disney Co</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>15.</td><td>FB - Facebook, Inc. Common Stock</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>16.</td><td>FDX - FedEx Corporation</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>17.</td><td>BEN - Franklin Resources, Inc.</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>18.</td><td>GS - Goldman Sachs Group Inc</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>19.</td><td>HLF - Herbalife Nutrition Ltd</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>20.</td><td>HD - Home Depot Inc</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>21.</td><td>IBM - IBM Common Stock</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>22.</td><td>INTC - Intel Corporation</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>23.</td><td>IVZ - Invesco Ltd.</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>24.</td><td>MSFT - Microsoft Corporation</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>25.</td><td>NFLX - Netflix Inc</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>26.</td><td>NBL - Nike Inc</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>27.</td><td>NBL - Noble Energy, Inc.</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>28.</td><td>PFE - Pfizer Inc.</td><td> </td><td> </td>
+      </tr>
+    </table>
+
+  </div>
+
+    <div class="p-4" style="border-style: solid">
+
+    <table border="1">
+      <tr>
+        <td colspan="4">1 Day Signals - UK Equity</td>
+      </tr>
+      <tr>
+	  	<td></td>
+        <td>Symbol</td>
+        <td>Date/Time</td>
+        <td>Details</td>
+      </tr>
+      <tr>
+        <td>1.</td><td></td><td> </td><td> </td>
+      </tr>
+    </table>
+
+  </div>
+
+  <div class="p-4" style="border-style: solid">
+
+    <table border="1">
+      <tr>
+        <td colspan="4">1 Day Signals - iShares ETFs</td>
+      </tr>
+      <tr>
+	  	<td></td>
+        <td>Symbol</td>
+        <td>Date/Time</td>
+        <td>Details</td>
+      </tr>
+      <tr>
+        <td>1.</td><td>iShares S&P Latin America 40 Index</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>2.</td><td>iShares MSCI Australia Index Fund</td><td> </td><td> </td>
+      </tr>
+	  <tr>
+        <td>3.</td><td>iShares MSCI Brazil Index</td><td> </td><td> </td>
+      </tr>
+    </table>
+
+  </div>
+
 </div>
