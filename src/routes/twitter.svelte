74a5e0562c646e62c640b940b52b0fa3df0cae6f
Eugene Murray <eugene.murray@gmail.com>
2019-11-09 18:31:01 +0000

    update twitter

--- src/routes/twitter.svelte
+++ src/routes/twitter.svelte
@@ -1,30 +1,41 @@
+<script
+  async
+  type="text/javascript"
+  src="https://platform.twitter.com/widgets.js"
+  charset="utf-8">
 
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-<script>
-    let showTwitter = false;
-    setTimeout(() => {
-        showTwitter = true;
-    }, 5000);
 </script>
 
-{#if showTwitter }
+<button
+  value="Refresh Page"
+  onClick="window.location.reload();"
+  type="button"
+  class="btn btn-success">
+  Refresh Twitter Feed
+</button>
+
 <div id="twitter-feeds" style="width: 100%">
-			
-    <table border="1" style="width: 100%" id="#twitter-feeds-table">
-        <tr>
-            <td valign="top">
-                <div style="height: 1000px; overflow-y: scroll;">
-                    <a class="twitter-timeline" 
-                    href="https://twitter.com/saxobank?ref_src=twsrc%5Etfw">
-                    Tweets by saxobank
-                    </a> </div> 	
-            </td>
-            <td valign="top">
-                    <div style="height: 1000px; overflow-y: scroll;">
-                    <a class="twitter-timeline" href="https://twitter.com/DailyFX?ref_src=twsrc%5Etfw">Tweets by DailyFX</a> 
-                </div> 	
-            </td>
-        </tr>
-    </table>
+
+  <table border="1" style="width: 100%" id="#twitter-feeds-table">
+    <tr>
+      <td valign="top">
+        <div style="height: 1000px; overflow-y: scroll;">
+          <a
+            class="twitter-timeline"
+            href="https://twitter.com/saxobank?ref_src=twsrc%5Etfw">
+            Tweets by saxobank
+          </a>
+        </div>
+      </td>
+      <td valign="top">
+        <div style="height: 1000px; overflow-y: scroll;">
+          <a
+            class="twitter-timeline"
+            href="https://twitter.com/DailyFX?ref_src=twsrc%5Etfw">
+            Tweets by DailyFX
+          </a>
+        </div>
+      </td>
+    </tr>
+  </table>
 </div>
-{/if}
